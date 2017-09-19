97c48a69d010d34754bdea60ef1da72efe60edb0
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-09-19 10:41:51 -0700

    Run format

--- packages/metal-soy-loader/src/index.js
+++ packages/metal-soy-loader/src/index.js
@@ -1,71 +1,67 @@
-/* global module, require */
-
-const {basename, resolve} = require('path')
-const fs = require('fs')
-const glob = require('glob')
-const metalsoy = require('metal-tools-soy')
-const rimraf = require('rimraf')
-const tmp = require('tmp')
+const { basename, resolve } = require('path');
+const fs = require('fs');
+const glob = require('glob');
+const metalsoy = require('metal-tools-soy');
+const rimraf = require('rimraf');
+const tmp = require('tmp');
 
 /**
  * metal-soy-loader
  */
-module.exports = function () {
-  const loaderCallback = this.async()
-  const tmpDir = tmp.dirSync()
-  let compilationError = null
+module.exports = function() {
+	const loaderCallback = this.async();
+	const tmpDir = tmp.dirSync();
+	let compilationError = null;
 
-  const templates = glob
-    .sync('**/*.soy')
-    .map((path) => resolve(path))
+	const templates = glob.sync('**/*.soy').map(path => resolve(path));
 
-  const src = templates
-    .filter((path) => !/node_modules/.test(path) && path !== this.resourcePath)
+	const src = templates.filter(
+		path => !/node_modules/.test(path) && path !== this.resourcePath,
+	);
 
-  const soyDeps = templates
-    .filter((path) => /node_modules/.test(path))
+	const soyDeps = templates.filter(path => /node_modules/.test(path));
 
-  // Its important that the current file it kept at
-  // the end of the src files. This way using same name in
-  // files does not produce an overwritten result.
-  src.push(this.resourcePath)
+	// Its important that the current file it kept at
+	// the end of the src files. This way using same name in
+	// files does not produce an overwritten result.
+	src.push(this.resourcePath);
 
-  /**
-   * Handles the compilation end.
-   * Emits an error if there where a problem during the compilation
-   * process or if there is no result file.
-   */
-  const handleEnd = () => {
-    let result = ''
+	/**
+	* Handles the compilation end.
+	* Emits an error if there where a problem during the compilation
+	* process or if there is no result file.
+	*/
+	const handleEnd = () => {
+		let result = '';
 
-    if (!compilationError) {
-      try {
-        result = fs.readFileSync(
-          `${tmpDir.name}/${basename(this.resourcePath)}.js`,
-          'utf-8'
-        )
-      } catch (error) {
-        compilationError = error
-      }
-    }
+		if (!compilationError) {
+			try {
+				result = fs.readFileSync(
+					`${tmpDir.name}/${basename(this.resourcePath)}.js`,
+					'utf-8',
+				);
+			} catch (error) {
+				compilationError = error;
+			}
+		}
 
-    rimraf.sync(tmpDir.name)
-    loaderCallback(compilationError, result)
-  }
+		rimraf.sync(tmpDir.name);
+		loaderCallback(compilationError, result);
+	};
 
-  /**
-   *
-   * @param error
-   */
-  const handleError = (error) => {
-    compilationError = error
-    handleEnd()
-  }
+	/**
+	 *
+	 * @param error
+	 */
+	const handleError = error => {
+		compilationError = error;
+		handleEnd();
+	};
 
-  metalsoy({
-    dest: tmpDir.name,
-    handleError,
-    soyDeps,
-    src
-  }).on('end', handleEnd)
-}
+	metalsoy({
+		dest: tmpDir.name,
+		handleError,
+		soyDeps,
+		src,
+	}).on('end', handleEnd);
+};
