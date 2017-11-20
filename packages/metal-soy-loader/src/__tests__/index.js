24d6bd112a082e71de685ef84b5ebd19d598b843
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-11-20 09:25:07 -0800

    Run format

--- packages/metal-soy-loader/src/__tests__/index.js
+++ packages/metal-soy-loader/src/__tests__/index.js
@@ -15,51 +15,59 @@ describe('metal-soy-loader', function() {
 	});
 
 	it('should run webpack without errors', done => {
-		webpack({
-			entry: path.join(__dirname, '../../test/fixtures/Parent.js'),
-			module: {
-				rules: [
-					{
-						test: /\.soy$/,
-						use: {
-							loader: path.join(__dirname, '../../src/index.js'),
-							options: {
-								src: 'test/fixtures/*.soy'
-							}
-						}
-					},
-					{
-						exclude: /(node_modules)/,
-						test: /\.js$/,
-						use: {
-							loader: 'babel-loader',
-							options: {
-								compact: false,
-								presets: ['babel-preset-es2015']
-							}
-						}
-					}
-				]
+		webpack(
+			{
+				entry: path.join(__dirname, '../../test/fixtures/Parent.js'),
+				module: {
+					rules: [
+						{
+							test: /\.soy$/,
+							use: {
+								loader: path.join(
+									__dirname,
+									'../../src/index.js'
+								),
+								options: {
+									src: 'test/fixtures/*.soy',
+								},
+							},
+						},
+						{
+							exclude: /(node_modules)/,
+							test: /\.js$/,
+							use: {
+								loader: 'babel-loader',
+								options: {
+									compact: false,
+									presets: ['babel-preset-es2015'],
+								},
+							},
+						},
+					],
+				},
+				output: {
+					filename: 'bundle.js',
+					library: 'metal',
+					libraryTarget: 'commonjs2',
+					path: outputDir,
+				},
 			},
-			output: {
-				filename: 'bundle.js',
-				library: 'metal',
-				libraryTarget: 'commonjs2',
-				path: outputDir
+			(error, stats) => {
+				expect(fs.existsSync(path.join(outputDir, 'bundle.js'))).toBe(
+					true
+				);
+				expect(stats.hasErrors()).toBe(false);
+				expect(stats.hasWarnings()).toBe(false);
+				expect(error).toBeNull();
+
+				const bundle = require(path.join(outputDir, 'bundle.js'));
+
+				const component = new bundle.Parent();
+
+				expect(component).toMatchSnapshot();
+
+				done();
 			}
-		}, (error, stats) => {
-			expect(fs.existsSync(path.join(outputDir, 'bundle.js'))).toBe(true);
-			expect(stats.hasErrors()).toBe(false);
-			expect(stats.hasWarnings()).toBe(false);
-			expect(error).toBeNull();
-
-			const bundle = require(path.join(outputDir, 'bundle.js'));
-
-			const component = new bundle.Parent();
-
-			expect(component).toMatchSnapshot();
-
-			done();
-		});
+		);
 	});
 });
