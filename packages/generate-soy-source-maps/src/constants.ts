76bf846e1bca91611b52ce8adfbf5bc10ba3a0bd
Matuzalém Teles <matuzalemteles@gmail.com>
2018-08-10 15:47:39 -0300

    Add constants

---
+++ packages/generate-soy-source-maps/src/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2018, Matuzalém Teles.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const SCall: string = 'Call';
+export const SInterpolation: string = 'Interpolation';
+export const SLetStatement: string = 'LetStatement';
+export const SParam: string = 'Param';
+export const SParamDeclaration: string = 'ParamDeclaration';
+export const STemplate: string = 'Template';
