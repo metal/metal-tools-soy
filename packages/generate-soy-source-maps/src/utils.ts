51e6c88ff1c8667c979d32d5aef6ef58f93352dc
Matuzalém Teles <matuzalemteles@gmail.com>
2018-08-10 15:44:07 -0300

    Add utils

---
+++ packages/generate-soy-source-maps/src/utils.ts
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2018, Matuzalém Teles.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { types as S } from 'soyparser';
+
+export function implTemplateName(name: string, namespace: (string | null)): string {
+    return `${namespace}.${name}`;
+}
+
+export function getLetName(name: string): string {
+    return name.split('__')[0];
+}
+
+export function isValidLetStatement(name: string): boolean {
+    return name.endsWith(getLetName(name)) && /soy+\d+/.test(name);
+}
+
+export function isValidTemplateName(name: string): boolean {
+    return name.startsWith(name);
+}
+
+export function closest(
+    type: string, 
+    ast: S.Program, 
+    whitelistParent: Array<string> = [], 
+    enter?: Function
+): void {
+    const parentNode: S.Node[] = []
+
+    const handlerDefault = (node: S.Node) => {
+        return node.type === type;
+    }
+
+    const getEnter = (handler: Function | undefined) => {
+        if (typeof handler === 'function') {
+            return handler;
+        } else {
+            return handlerDefault;
+        }
+    }
+
+    const traverse = (node: S.Node, depth: number = 0) => {
+        if (!node) return;
+
+        if (node.type === type) {
+            if (getEnter(enter)(node, parentNode[depth - 1])) {
+                return;
+            }
+        };
+
+        if (whitelistParent.includes(node.type)) {
+            parentNode.push(node);
+
+            if (node.body && Array.isArray(node.body)) {
+                depth++;
+            }
+        }
+
+        if (node.body) {
+            if (Array.isArray(node.body)) {
+                node.body.forEach((node: S.Node) => traverse(node, depth));
+            } else {
+                traverse(node.body, depth);
+            }
+        }
+    };
+
+    traverse(ast);
+}
+
+export const getKeys = <T extends {}>(o: T): Array<keyof T> => <Array<keyof T>>Object.keys(o)
